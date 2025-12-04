ef5a813b25dde9f89ea0c397390e50d6c65b7519
SaleamlakH <saleamlak.7h@gmail.com>
2025-12-04 18:34:13 +0300

    Write a function that play a single round

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,3 +1,6 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
     let randomNumber = Math.floor(Math.random() * 3) + 1;
 
@@ -20,3 +23,37 @@ function getHumanChoice() {
     return 'scissors';
 }
 
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice == computerChoice) {
+        console.log('%cIt\'s tie', 'color: yellow;');
+    } else if (humanChoice == 'rock') {
+        if (computerChoice == 'paper') {
+            console.log('%cYou lose, paper wraps rock.', 'background-color: red;');
+            computerScore++;
+        } else {
+            console.log('%cYou win, rock breaks scissors.', 'background-color: green;');
+            humanScore++;
+        }
+    } else if (humanChoice == 'paper') {
+        if (computerChoice == 'rock') {
+            console.log('%cYou win, paper wraps rock.', 'background-color: green;');
+            humanScore++;
+        } else {
+            console.log('%cYou lose, scissors cut paper.', 'background-color: red;');
+            computerScore++;
+        }
+    } else {
+        if (computer == 'rock') {
+            console.log('%cYou lose, rock breaks scissors.', 'background-color: red;');
+            computerScore++;
+        } else {
+            console.log('%cYou win, scissors cut paper.', 'background-color: green');
+            humanScore++;
+        }
+    }
+}
+
+const humanChoice = getHumanChoice();
+const computerChoice = getComputerChoice();
+
+playRound(humanChoice, computerChoice);
